5cfa92a47b06ae7428c8220769d227f5a249c683
lory2k01 <tecnolory01@gmail.com>
2021-12-20 21:40:44 +0100

    - setting up working tree

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import "../src/config/firebase.config";
+import { AuthProvider } from '../src/hook/auth';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />;
+    </AuthProvider>
+  )
 }
 
 export default MyApp
---
+++ src/service/AuthService.js
@@ -0,0 +1,22 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+const AuthService = {
+    loginWithGoogle: async () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        try {
+            const userCred = await firebase.auth().signInWithPopup(provider);
+            return {
+                user: userCred,
+            };
+        } catch (e) {
+            return {
+                error: e.message,
+            };
+        }
+    },
+    logout: async () => {
+        await firebase.auth().signOut();
+    },
+};
+
+export default AuthService;
